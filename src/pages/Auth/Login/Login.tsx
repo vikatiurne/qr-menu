d804cd18ece3c77c7b3e1c08f1b2b4b8924ab0e5
unknown <https://github.com/TihoH>
2025-01-15 11:25:41 +0200

    edit location fils

--- src/pages/Auth/Login/Login.tsx
+++ src/pages/Auth/Login/Login.tsx
@@ -1,5 +1,5 @@
 import Container from "@/hoc/Container/Container";
-import LoginForm from "../../../components/Login/LoginForm";
+import LoginForm from "../../../components/Auth/Login/LoginForm";
 
 const Login: React.FC = () => {
   return <Container>
