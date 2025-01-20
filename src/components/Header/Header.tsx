040e758e8bf4149c8a57a59f57658755f5b5644f
Tiurne Viktoriya <tiurne@ukr.net>
2025-01-20 17:14:37 +0200

    add customBtn with gradient

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import Button from '@/components/UI/Button';
 import LoginIcon from '@/assets/Header/icon.svg';
+import CustomButton from '../UI/CustomButton';
 
 const Header: React.FC = () => {
   const location = useLocation();
@@ -38,9 +39,18 @@ const Header: React.FC = () => {
                 Увійти
               </span>
             </Button> */}
-            <Button className="relative text-lg font-raleway w-[7.625rem] h-[2.75rem] bg-white rounded-[.625rem] border-none py-[.625rem] px-8 before:content-[''] before:absolute before:inset-0 before:bg-gradient-to-t before:from-[#15C5CE] before:to-[#0C677C] before:rounded-[.75rem] before:m-[-0.2rem] before:z-[-1]">
+            {/* <Button className="relative text-lg font-raleway w-[7.625rem] h-[2.75rem] bg-white rounded-[.625rem] border-none py-[.625rem] px-8 before:content-[''] before:absolute before:inset-0 before:bg-gradient-to-t before:from-[#15C5CE] before:to-[#0C677C] before:rounded-[.75rem] before:m-[-0.2rem] before:z-[-1]">
               Увійти
-            </Button>
+            </Button> */}
+
+            <CustomButton
+              className="w-[7.625rem] h-[2.75rem] mobile:hidden"
+              borderRadius="rounded-[.625rem]"
+              whiteBtn
+              onClick={() => console.log('Login')}
+            >
+              <span className="text-lg font-raleway text-black ">Увійти</span>
+            </CustomButton>
           </Link>
         )}
       </div>
