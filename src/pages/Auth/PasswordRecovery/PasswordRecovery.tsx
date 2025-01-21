2bd87acecaea885fe766ff55f41fcd945e0aa0b9
Jenya Golovko <u9on1210@gmail.com>
2025-01-21 20:31:00 +0200

    third step PasswordRecovery

--- src/pages/Auth/PasswordRecovery/PasswordRecovery.tsx
+++ src/pages/Auth/PasswordRecovery/PasswordRecovery.tsx
@@ -23,17 +23,19 @@ const PasswordRecovery: React.FC = () => {
     console.log('Sending password reset email to:', data.email);
   };
   return (
-    <div className="w-[432px] flex flex-col items-center">
+    <div className="w-full max-w-[432px] mobile:max-w-[343px] flex flex-col items-center">
       <img src={logo} alt="Info" className="w-[58px] h-[58px] m-[24px]" />
-      <h1 className="text-[31px] h-[41] w-[224] text-bold font-serif font-semibold">
+      <h1 className="w-full text-[27px] h-[41px] max-w-[224px] font-serif font-bold">
         Reset Password
       </h1>
-      <p className="mt-[32px] w-[390px] text-[16px] font-medium text-[#959895] text-left-center">
-        Enter your email address and we’ll send you an email with instructions
-        to reset your password
-      </p>
-      <div className="mt-[82px]">
-        <p className="ml-[3px] font-medium text-[#4B4B4B]">Enter</p>
+      <div className="w-full h-[98px] mobile:h-[72px] mt-[32px] flex">
+        <p className="max-w-[390px] ml-[9px] text-[16px] font-medium text-[#959895] text-center">
+          Enter your email address and we’ll send you an email with instructions
+          to reset your password
+        </p>
+      </div>
+      <div className="mt-[32px] w-full max-w-[423px] mobile:max-w-[343px] mobile:mt-[24px] mobile:ml-4">
+        <p className="ml-[3px] mobile:ml-0 font-medium text-[#4B4B4B]">Enter</p>
         <form onSubmit={handleSubmit(onReset)} className="mt-[9px]">
           <Controller
             name="email"
@@ -46,12 +48,12 @@ const PasswordRecovery: React.FC = () => {
               },
             }}
             render={({ field }) => (
-              <div className="relative w-[423px] h-[4rem]">
+              <div className="relative w-full  h-[4rem]">
                 <input
                   {...field}
                   type="email"
                   placeholder="Enter your email"
-                  className={`w-full h-full pl-[27px] border-solid border-[1px]
+                  className={`w-full h-full pl-[27px] mobile:pl-[18px] border-solid border-[1px]
                   ${errors.email ? 'border-[#F64C4C]' : 'border-[#15C5CE]'} rounded-md focus:outline-none`}
                 />
                 {errors.email && (
@@ -62,7 +64,7 @@ const PasswordRecovery: React.FC = () => {
                       className="absolute right-[19px] top-[50%] transform -translate-y-1/2 w-[20px] h-[20px]"
                     />
 
-                    <p className="text-[#F64C4C] font-normal text-[14px] mt-[6px] pb-[21px]">
+                    <p className="text-[#F64C4C] font-normal text-[14px] mt-[6px] pb-[21px] mobile:pb-0 mobile:mt-[2px]">
                       {errors.email.message}
                     </p>
                   </>
@@ -72,7 +74,7 @@ const PasswordRecovery: React.FC = () => {
           />
           <button
             type="submit"
-            className={`w-[423px] h-[57px] align-middle bg-gradient-to-b from-[#0C677C] to-[#15C5CE] rounded-md text-white text-base font-medium ${errors.email ? 'mt-[67px]' : 'mt-[32px]'}`}
+            className={`w-full h-[57px] align-middle bg-gradient-to-b from-[#0C677C] to-[#15C5CE] rounded-md text-white text-base font-medium ${errors.email ? 'mt-[67px]' : 'mt-[32px]'} mobile:mt-[24px]`}
           >
             Reset
           </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}", 
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
-        'custom-gradient': 'linear-gradient(0deg, rgba(21,197,206,1) 0%, rgb(14, 129, 155) 100%)',
+        'custom-gradient':
+          'linear-gradient(0deg, rgba(21,197,206,1) 0%, rgb(14, 129, 155) 100%)',
       },
-    }
+
+      screens: {
+        mobile: { min: '375px', max: '768px' },
+      },
+    },
   },
   plugins: [],
-}
+};
