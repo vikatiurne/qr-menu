722b20409f95f83dbfed6bd1a0e3f12d2a622c2d
Goose <arabadzhi.artem@gmail.com>
2025-01-23 17:58:53 +0200

    Add gradient to tailwind.conf and fix Hero

--- src/components/Home/Hero/Hero.tsx
+++ src/components/Home/Hero/Hero.tsx
@@ -3,56 +3,63 @@ import { Link } from 'react-router-dom';
 import HeroCardImgLt from '@/assets/Home/cardImgLt.png';
 import HeroCardImgRt from '@/assets/Home/cardImgRt.png';
 
-const TxtGradientStyle = {
-  background: 'linear-gradient(to top, #15C5CE, #0C677C)',
-  WebkitBackgroundClip: 'text',
-  WebkitTextFillColor: 'transparent',
-};
 const Hero: React.FC = () => {
   return (
     <div className="grid grid-rows-2 font-elMassiri text-center text-[#2B2A34]">
       <div className="container mx-auto my-10">
-        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2 lg:auto-rows-fr lg:gap-6">
-          <div className=" flex flex-col  justify-between mb-10">
-            <p className="font-bold 2xl:text-5xl xl:text-4xl lg:text-3xl md:text-4xl text-3xl">
+        <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
+          {/* Part 1 */}
+          <div className="">
+            <p className="font-bold 2xl:text-6xl xl:text-5xl lg:text-4xl md:text-3xl text-4xl xl:mb-0 mb-10">
               Розумні
-              <span style={TxtGradientStyle}> інтсрументи для успіху </span>
+              <span className="bg-gradient-to-top text-transparent bg-clip-text">
+                {' '}
+                інтсрументи для успіху{' '}
+              </span>
               вашого ресторану
             </p>
-            <div className="text-white flex justify-center">
-            <Link to="#JoinUs">
+          </div>
+          {/* part2 rghtImg*/}
+          <div className="lg:row-span-2 flex justify-center items-center object-contain">
+            <img
+              className="w-full  h-full object-cover rounded-3xl"
+              src={HeroCardImgRt}
+              alt="cardImgRgt"
+            />
+          </div>
+          {/* part 3 */}
+          <div className="grid grid-cols-1 grid-rows-[auto,1fr] items-center justify-center">
+            {/* 1 row - button + link*/}
+            <Link
+              className="w-[max-content] h-[max-content] bg-custom-gradient xl:rounded-xl  lg:rounded-xl rounded-xl  mx-auto my-12"
+              to="#JoinUs"
+            >
               <CustomButton
-                className="px-8 py-1 text-2xl"
+                className="xl:px-9 xl:py-3 xl:text-2xl lg:px-12 lg:py-4 lg:text-4xl  px-10 py-2 text-3xl text-white bg-none "
                 borderRadius="rounded-lg"
                 whiteBtn={false}
               >
-                Спробувати
+                СПРОБУВАТИ
               </CustomButton>
-              </Link>
-            </div>
-          </div>
-          <div className="lg:row-span-2  flex justify-center items-center">
-            <img className="" src={HeroCardImgRt} alt="cardImgRgt" />
-          </div>
-          <div className=" flex items-end justify-center">
-            <div className="grid grid-cols-2 gap-4 ">
-              <img
-                className="w-full mobile:w-[4rem]"
-                src={HeroCardImgLt}
-                alt="cardImgLft"
-              />
-              <p className="text-white text-center xl:text-2xl lg:text-xl flex items-center px-2 bg-gradient-to-t from-[#15C5CE] to-[#0C677C] border-none rounded-3xl overflow-hidden">
+            </Link>
+            {/* 2 row - img+txt */}
+            <div className="grid grid-cols-2 gap-4 mt-auto">
+              <img className="w-full" src={HeroCardImgLt} alt="cardImgLft" />
+              <p className="text-white text-center xl:text-3xl lg:text-4xl md:text-3xl  text-2xl flex items-center px-2 bg-gradient-to-top border-none rounded-3xl overflow-hidden">
                 Оптимізуйте ресторанний бізнес із Velori
               </p>
             </div>
           </div>
         </div>
       </div>
-
-      <p className="text-4xl font-bold">
-        Velori перетворює меню на <span style={TxtGradientStyle}>QR-код</span>,
-        а<span style={TxtGradientStyle}>AI-офіціант</span> допомагає обрати
-        страви за вподобаннями.
+      <p className="text-3xl xl:text-4xl lg:text-3xl md:text-2xl font-bold w-[90%] mx-auto mt-10 h-[max-content]">
+        Velori перетворює меню на{' '}
+        <span className="gradient-to-top">QR-код</span>, а
+        <span className="bg-gradient-to-top text-transparent bg-clip-text">
+          {' '}
+          AI-офіціант{' '}
+        </span>
+        допомагає обрати страви за вподобаннями.
       </p>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      backgroundImage: {
+        'gradient-to-top': 'linear-gradient(to top, #15C5CE, #0C677C)',
+        'custom-gradient': 'linear-gradient(298deg, #15C5CE 31.01%, #0B5768 83.54%)',
+      },
       fontFamily: {
         raleway: ['Raleway', 'sans-serif'],
         elMassiri: ['El Messiri', 'sans-serif'],
