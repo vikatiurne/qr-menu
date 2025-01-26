ee58b5a0059d22ebb0d38da031f98ebd25b23508
Tiurne Viktoriya <tiurne@ukr.net>
2025-01-26 13:00:58 +0200

    fix align-items,img size, padding top for login/redistr

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import LoginIcon from '@/assets/Header/icon.svg';
 import CustomButton from '../UI/CustomButton';
 
 const Header: React.FC = () => {
   const [language, setLanguage] = useState('UA');
-  const location = useLocation();
+
+  const { pathname } = useLocation();
+
+  const [isAuth, setIsAuth] = useState(
+    pathname.toLocaleLowerCase() === '/login' ||
+      pathname.toLocaleLowerCase() === '/registration'
+  );
+
+  useEffect(() => {
+    setIsAuth(pathname === '/login' || pathname === '/registration');
+  }, [pathname]);
 
   const setUkrainianLanguage = () => setLanguage('UA');
   const setEnglishLanguage = () => setLanguage('EN');
 
   return (
-    <header className="w-full h-full flex justify-between">
+    <header
+      className={`w-full h-full flex ${isAuth ? 'items-center pt-[1.375rem]' : ''} justify-between`}
+    >
       <img
-        className="w-[9rem] h-[5.875rem] mobile:w-[4.375rem] mobile:h-[3.125rem]"
+        className={`${isAuth ? 'w-[5.625rem] h-[4.25rem]' : 'w-[9rem] h-[5.875rem]'} mobile:w-[4.375rem] mobile:h-[3.125rem]`}
         src="/logo.svg"
         alt="Logo"
       />
@@ -44,7 +56,7 @@ const Header: React.FC = () => {
             UA
           </CustomButton>
         </div>
-        {location.pathname === '/' && (
+        {pathname === '/' && (
           <Link to="login">
             <button onClick={() => console.log('Login')}>
               <img
