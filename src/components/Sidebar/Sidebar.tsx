e80d6cedb396ff8812d8793eb3df9535c035264c
unknown <https://github.com/TihoH>
2025-03-20 12:30:31 +0200

    edit style border sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,15 +3,14 @@ import ListNavigate from '../UI/ListNavigate';
 import { useLocation } from 'react-router-dom';
 
 const Sidebar: React.FC = () => {
-  const {pathname} = useLocation()
-  const appSidebar = pathname === '/restaurant/sidebar' ? true : false
+  const { pathname } = useLocation();
+  const appSidebar = pathname === '/restaurant/sidebar' ? true : false;
 
   return (
-    
-    <div className={`${ !appSidebar ? 'border-r-2 border-gray-200 px-2 min-w-[353px]' : 'w-full'} flex items-center  flex-col justify-center  `}>
-      <ListNavigate
-        typeSidebar={appSidebar}
-      />
+    <div
+      className={`${!appSidebar ? 'border-r-2 border-gray-200 px-2 min-w-[353px]' : 'w-full'} flex items-center  flex-col `}
+    >
+      <ListNavigate typeSidebar={appSidebar} />
     </div>
   );
 };
